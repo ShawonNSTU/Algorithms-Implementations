b1de6e3f3a6a4fa396201c971233e3f24b3ee122
ShawonNSTU <shuvroshawon9@gmail.com>
2016-08-22 16:19:44 +0600

    Update Counting_digit_in_the_factorial_of_a_number.cpp

--- Counting_digit_in_the_factorial_of_a_number.cpp
+++ Counting_digit_in_the_factorial_of_a_number.cpp
@@ -11,7 +11,7 @@ int main()
 /*
     Here,
 
-        log10(1) = 0 because 10^0 = 1, log10(10)=1 because 10^1 = 10, log(100)=2 because 10^2 = 100. . .
+        log10(1) = 0 because 10^0 = 1, log10(10)=1 because 10^1 = 10, log10(100)=2 because 10^2 = 100. . .
         log10() works with the power of 10. . .
 
     Formula: 6! = log10(1) + log10(2) + log10(3) + log10(4) + log10(5) + log10(6)
