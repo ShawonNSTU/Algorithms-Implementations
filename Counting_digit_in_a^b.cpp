85611add37639a07548732a687b8091796dbf691
ShawonNSTU <shuvroshawon9@gmail.com>
2016-08-22 16:44:19 +0600

    Update Counting_digit_in_a^b.cpp

--- Counting_digit_in_a^b.cpp
+++ Counting_digit_in_a^b.cpp
@@ -15,7 +15,7 @@ int main()
         log10(1) = 0 because 10^0 = 1, log10(10)=1 because 10^1 = 10, log(100)=2 because 10^2 = 100. . .
         log10() works with the power of 10. . .
 
-    Formula: 4^5 = log10(4)+log10(4)+log10(4)+log10(4)+log10(4) = 4
+    Formula: 4^5 = log10(4)+log10(4)+log10(4)+log10(4)+log10(4)
 */
     for(int i=1;i<=b;i++)
         digit+=log10(a);
