5aea2f87b760d8fc0197addd7655721f9c934d38
ShawonNSTU <shuvroshawon9@gmail.com>
2017-06-24 20:06:46 +0600

    Add files via upload

--- light oj 1210(graph).cpp
+++ light oj 1210(graph).cpp
@@ -1,80 +1,85 @@
 #include <bits/stdc++.h>
+#define mx 50005
 using namespace std;
-const int MAX=50005;
 typedef vector<int> vi;
-vi S,AdjList[MAX];
-int dfs_num[MAX],dfs_low[MAX],visited[MAX],belong[MAX];
-int flag[MAX],in[MAX],out[MAX];
-int numSCC,dfsNumberCounter,SCC;
-void tarjanSCC(int u)
+vi ts,AdjList[mx],RevList[mx];
+int SCC[mx],in[mx],out[mx];
+bool visited[mx];
+void dfs(int u)
 {
-    dfs_low[u]=dfs_num[u]=++dfsNumberCounter;
-    S.push_back(u);
     visited[u]=1;
-    for(int j=0;j<(int)AdjList[u].size();j++){
-        int v=AdjList[u][j];
-        if(dfs_num[v]==0) tarjanSCC(v);
-        if(visited[v]==1)
-            dfs_low[u]=min(dfs_low[u],dfs_low[v]);
-    }
-    if(dfs_low[u]==dfs_num[u]){                  // if this is a root (start) of an SCC
-        while(1){
-            int v=S.back(); S.pop_back();visited[v]=0;
-            belong[v]=SCC;
-            if(u==v) break;
-        }
-        SCC++;
-    }
-}
-void dfs2(int u)
-{
-    flag[u]=1;
-    for(int i=0;i<AdjList[u].size();i++){
+    for(int i=0;i<(int)AdjList[u].size();i++){
         int v=AdjList[u][i];
-        if(belong[u]!=belong[v]){
-            in[belong[v]]++;
-            out[belong[u]]++;
-        }
-        if(flag[v]==0) dfs2(v);
+        if(!visited[v]) dfs(v);
+    }
+    ts.push_back(u);
+}
+void findSCC(int u,int mark)
+{
+    SCC[u]=mark;
+    visited[u]=1;
+    for(int i=0;i<(int)RevList[u].size();i++){
+        int v=RevList[u][i];
+        if(!visited[v]) findSCC(v,mark);
     }
 }
-void Clear(int N)
+void Count(int u)
 {
-    for(int i=1;i<=N+2;i++){
-        dfs_num[i]=0; dfs_low[i]=0; visited[i]=0;
-        belong[i]=0; flag[i]=0;
-        in[i]=0; out[i]=0;
-        AdjList[i].clear();
+    visited[u]=1;
+    for(int i=0;i<(int)AdjList[u].size();i++){
+        int v=AdjList[u][i];
+        if(SCC[u]!=SCC[v]){
+            in[SCC[v]]++;
+            out[SCC[u]]++;
+        }
+        if(!visited[v]) Count(v);
     }
+}
+void Clear(int n)
+{
+    for(int i=1;i<=n;i++){
+        AdjList[i].clear();
+        RevList[i].clear();
+        SCC[i]=out[i]=in[i]=0;
+    }
+    ts.clear();
 }
 int main()
 {
-    int N,E,u,v;
     int T;
     scanf("%d",&T);
-    for(int k=1;k<=T;k++){
-    scanf("%d %d",&N,&E);
-    Clear(N);
-    for(int i=1;i<=E;i++){
-        scanf("%d %d",&u,&v);
-        AdjList[u].push_back(v);
-    }
-    dfsNumberCounter=numSCC=SCC=0;
-    for(int i=1;i<=N;i++){
-        if(dfs_num[i]==0) tarjanSCC(i);
-    }
-    for(int i=1;i<=N;i++){
-        if(flag[i]==0){
-            dfs2(i);
+    for(int t=1;t<=T;t++){
+        int n,m,u,v,mark=0;
+        scanf("%d %d",&n,&m);
+        for(int i=1;i<=m;i++){
+            scanf("%d %d",&u,&v);
+            AdjList[u].push_back(v);
+            RevList[v].push_back(u);
         }
-    }
-    int mIn,mOut;
-    mIn=mOut=0;
-    for(int i=0;i<SCC&&SCC>1;i++){
-        if(in[i]==0) ++mOut;
-        if(out[i]==0) ++mIn;
-    }
-    printf("Case %d: %d\n",k,max(mOut,mIn));
+        memset(visited,0,sizeof(visited));
+        for(int i=1;i<=n;i++){
+            if(!visited[i]) dfs(i);
+        }
+        reverse(ts.begin(),ts.end());
+        memset(visited,0,sizeof(visited));
+        for(int i=0;i<(int)ts.size();i++){
+            if(!visited[ts[i]]){
+                ++mark;
+                findSCC(ts[i],mark);
+            }
+        }
+        memset(visited,0,sizeof(visited));
+        for(int i=1;i<=n;i++){
+            if(!visited[i]) Count(i);
+        }
+        int foo,curr;
+        foo=curr=0;
+        for(int i=1;i<=mark&&mark>1;i++){
+            if(in[i]==0) ++foo;
+            if(out[i]==0) ++curr;
+        }
+        printf("Case %d: %d\n",t,max(foo,curr));
+        Clear(n);
     }
     return 0;
 }
